0ed768cf2f47160c41f37bd77ee062fadbb0f4a1
James <jamesmcdaniel33@hotmail.com>
2023-06-21 17:38:59 -0500

    Exercise 4 & 5

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-console.log("Hello World!\n==========\n");
+console.log("Hello Reviewer!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log("EXERCISE 1: Printing Odd Numbers\n==========\n");
 
 printOdds(-33);
 
@@ -22,11 +22,11 @@ function printOdds(count) {
 }
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 2: Checking Age\n==========\n");
 
 checkAge(18);
 checkAge(30, "James");
-checkAge;
+checkAge(13, "Addison");
 
 function checkAge(age, name) {
     let above16 = `Congrats ${name}, you can drive!`
@@ -40,7 +40,7 @@ function checkAge(age, name) {
 }
 
 // Exercise 3 Section
-console.log("EXERCISE 3:\n==========\n");
+console.log("EXERCISE 3: Checking Coordinates\n==========\n");
 
 onAxis(0,0);
 onAxis(1,0);
@@ -69,5 +69,69 @@ function onAxis(x, y) {
 }
 
 // Exercise 4 Section
-console.log("EXERCISE 4:\n==========\n");
+console.log("EXERCISE 4: Checking Triangles\n==========\n");
 
+isTriangle(1,2,3);
+isTriangle(1,1,1);
+isTriangle(1,2,2);
+isTriangle(2,3,4);
+
+function isTriangle(a,b,c) {
+    if(a + b > c && a + c > b && b + c > a) {
+        if(a === b && b === c) {
+            console.log(`Side lengths ${a}, ${b}, and ${c} form an equilateral triangle.`)
+        } else if (a === b || a === c || b === c ) {
+            console.log(`Side lengths ${a}, ${b}, and ${c} form an isosceles triangle.`)
+        } else {
+            console.log(`Side lengths ${a}, ${b}, and ${c} form a scalene triangle.`)
+        }
+    } else {
+        console.log(`Side lengths ${a}, ${b}, and ${c} do not form a triangle.`);
+    }
+}
+
+// Exercise 5 Section
+console.log("EXERCISE 5: Checking Data Plan Status\n==========\n");
+
+console.log("Case Over: plan limit = 100 GB, days remaining = 15, usage = 60 GB")
+dataPlanStatus(100, 15, 60);
+console.log("==========\n");
+console.log("Case Under: plan limit = 100 GB, days remaining = 15, usage = 40 GB");
+dataPlanStatus(100, 15, 40);
+console.log("==========\n");
+console.log("Case On Average: plan limit = 100 GB, days remaining = 15, usage = 50 GB");
+dataPlanStatus(100, 15, 50);
+console.log("==========\n");
+console.log("Case Over Limit: plan limit = 100 GB, days remaining = 15, usage = 150 GB");
+dataPlanStatus(100, 15, 150);
+
+function dataPlanStatus(planLimit, day, usage) {
+    
+    if(usage > planLimit) {
+        console.log("You are out of data.");
+    } else {      
+        let planAvg = planLimit / 30;
+        let userAvg = usage / day;
+        let daysRemaining = 30 - day;
+        let dataRemaining = planLimit - usage;
+    
+        console.log(`${day} days used, ${daysRemaining} days remaining`);
+        console.log(`Average daily use: ${userAvg} GB/day`);
+        console.log(`${dataRemaining} GB remaining`);
+    
+        let potentialOverage = ((userAvg * 30) - (planAvg * 30));
+        let correctionAvg = dataRemaining / daysRemaining;
+        
+        if (userAvg > planAvg) {
+            console.log(`You are EXCEEDING your plan's daily average data use of ${planAvg} GB/day.`);
+            console.log(`Continued usage at the current rate will result in an overage of ${potentialOverage} GB.`);
+            console.log(`In order to not go over your limit of ${planLimit} GB, use no more than ${correctionAvg} GB/day.`);
+        } else if (userAvg < planAvg) {
+            console.log(`You are BELOW your plan's daily average data use of ${planAvg} GB/day.`);
+            console.log(`Continued usage at this rate will result in ${Math.abs(potentialOverage)} GB left over.`);
+            console.log(`You can use up to ${correctionAvg} GB/day for the remaining ${daysRemaining} days.`);
+        } else {
+            console.log(`You are on track with your plan's daily average data use of ${planAvg} GB/day.`);
+        }
+    }
+}
